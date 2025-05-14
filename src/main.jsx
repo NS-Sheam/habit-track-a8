5d4ce722928fcfdaeea2b460e218c44c3316cbd4
NS-Sheam <123sheamfeni@gmail.com>
2025-05-14 17:25:07 +0600

    add: nav in main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 
@@ -9,6 +11,8 @@ import Home from "./pages/Home";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Navbar />
     <Home />
+    <Footer />
   </StrictMode>
 );
